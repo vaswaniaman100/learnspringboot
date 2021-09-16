a56a6f11c178a56a35542408a956ef57c32b57c6
Aman <vaswaniaman100@gmail.com>
2021-09-16 15:16:47 +0530

    commit

---
+++ ApiWithHibernate/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	int id;
+	@Column(name="name")
+	String name;
+	@Column(name="address")
+	String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
---
+++ ApiWithHibernate/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,53 @@
+package com.example.demo.service;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.respository.EmployeeRepository;
+
+
+@Service
+public class EmployeeService {
+	@Autowired
+	JdbcTemplate jdbc;
+	
+	@Autowired
+	EmployeeRepository rep;
+	
+	
+	Employee emp = new Employee();
+	public void insert(String name,String address) {
+		emp.setName(name);
+		emp.setAddress(address);
+		rep.save(emp);
+		
+	
+	}
+	public void insertorupdate(int id, String name,String address) {
+		emp.setId(id);
+		emp.setName(name);
+		emp.setAddress(address);
+		rep.save(emp);
+		
+	}
+	public void delete(int id) {
+		rep.deleteById(id);
+		
+	}
+	
+	
+	public List<Employee> getemployees(){
+		return rep.getemployeelist();
+	}
+}
